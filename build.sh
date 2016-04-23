7988bb699470598a1a424d055de0863e5c283ae5
Michael Jumper <mjumper@apache.org>
2016-04-23 16:51:28 -0700

    Add easy mechanism for starting local webserver.

--- build.sh
+++ build.sh
@@ -7,24 +7,64 @@ log() {
 assert_directory() {
     NAME="$1"
     if [ ! -d doc ]; then
-        log "FATAL: $NAME does not exist or is not a directory."
+        log "FATAL: \"$NAME\" does not exist or is not a directory."
         exit 1
     fi
 }
 
-# Build site
-jekyll build
+assert_program() {
+    NAME="$1"
+    if ! which "$NAME" &> /dev/null; then
+        log "FATAL: \"$NAME\" is not installed."
+        exit 1
+    fi
+}
 
-# Verify expected directories exist
-assert_directory doc
-assert_directory _site
+build() {
 
-# Clean out content directory (if present)
-rm -rf content/; mkdir content
+    # Verify required programs are installed
+    assert_program jekyll
 
-# Copy site and docs into place
-cp -a doc/ content/
-cp -a _site/* content/
+    # Build site
+    jekyll build
 
-# Done
+    # Verify expected directories exist
+    assert_directory doc
+    assert_directory _site
+
+    # Clean out content directory (if present)
+    rm -rf content/; mkdir content
+
+    # Copy site and docs into place
+    cp -a doc/ content/
+    cp -a _site/* content/
+
+}
+
+serve() {
+
+    # Verify required programs are installed
+    assert_program ruby
+
+    # Run server
+    ruby -run -e httpd content/ -p "$1"
+
+}
+
+# Verify number of arguments
+if [ "$#" -gt 1 -o "$1" = "-h" ]; then
+    log "Usage:"
+    log "    $0 -h       # Display this message"
+    log "    $0          # Build website"
+    log "    $0 PORT     # Build website and serve from the given PORT"
+    exit 1
+fi
+
+# Build in all cases
+build
+
+# Serve on requested port
+if [ -n "$1" ]; then
+    serve "$1"
+fi
 
