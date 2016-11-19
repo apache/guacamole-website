06386843fb272f6679c029bb52784603d2953626
Michael Jumper <mjumper@apache.org>
2016-11-18 20:05:20 -0800

    Deploy draft documentation and release notes for 0.9.10-incubating.

---
+++ content/doc/0.9.10-incubating/libguac/search/variables_10.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['unlock_5fhandler',['unlock_handler',['../structguac__socket.html#a67be8367b5e1b330ee151b22c692500b',1,'guac_socket']]],
+  ['user_5fid',['user_id',['../structguac__user.html#aad1c6a8b56d17c12eb1f1e36c4798c70',1,'guac_user']]]
+];
