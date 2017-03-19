b8b84d9602f30ba404f53f46a2d1250a60cec971
Michael Jumper <mjumper@apache.org>
2017-03-18 19:33:47 -0700

    Add documentation for 0.9.12-incubating.

---
+++ doc/0.9.12-incubating/libguac/search/variables_f.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['select_5fhandler',['select_handler',['../structguac__socket.html#ad29e7f8a6819ca7bb541e8a947d5bc4f',1,'guac_socket']]],
+  ['size_5fhandler',['size_handler',['../structguac__user.html#a36a2fee906be0898907ba6e4a97bdb9a',1,'guac_user']]],
+  ['socket',['socket',['../structguac__client.html#aa634856fd3c522377ba7345f3308000e',1,'guac_client::socket()'],['../structguac__user.html#a8496638b8c4c52f5f15d483b92becf27',1,'guac_user::socket()']]],
+  ['state',['state',['../structguac__client.html#acb3e3f5d0ce5726cdde768f97b473f53',1,'guac_client::state()'],['../structguac__parser.html#a2773500f91b3cdb7de7bdea280228054',1,'guac_parser::state()'],['../structguac__socket.html#ac2d5236c7e570dfee207511241edaa9b',1,'guac_socket::state()']]],
+  ['stream',['stream',['../structguac__audio__stream.html#a4d8f5cfc70ec54b97ba0c4eea596f894',1,'guac_audio_stream']]],
+  ['sync_5fhandler',['sync_handler',['../structguac__user.html#a209e83f080baf2e84d04cf08395374d2',1,'guac_user']]]
+];
