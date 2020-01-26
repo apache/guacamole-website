eaa291ada94e6f8f691438e7bc079399638ef859
Michael Jumper <mjumper@apache.org>
2020-01-26 10:20:28 -0800

    Add documentation for 1.1.0.

---
+++ doc/1.1.0/libguac/search/all_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['begin_5fhandler',['begin_handler',['../structguac__audio__encoder.html#a43d161556d3d55031ea486cc8b4ce3f6',1,'guac_audio_encoder']]],
+  ['blob_5fhandler',['blob_handler',['../structguac__stream.html#a51122ba04f416d8ce047542c603632f8',1,'guac_stream::blob_handler()'],['../structguac__user.html#a88a14fda25c9929269d198d4d602f7fb',1,'guac_user::blob_handler()']]],
+  ['bps',['bps',['../structguac__audio__stream.html#a1de1be8874fb844002b92ac1e3813866',1,'guac_audio_stream']]]
+];
