76da8e29280da8cedc3e490a73bec734e7894247
Michael Jumper <mjumper@apache.org>
2020-06-25 20:24:13 -0700

    Add documentation for 1.2.0.

---
+++ doc/1.2.0/libguac/search/all_14.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['wol_2dconstants_2eh',['wol-constants.h',['../wol-constants_8h.html',1,'']]],
+  ['wol_2eh',['wol.h',['../wol_8h.html',1,'']]],
+  ['write_5fhandler',['write_handler',['../structguac__audio__encoder.html#a1b74c3dd402b0a6afa5f5b9000d02c74',1,'guac_audio_encoder::write_handler()'],['../structguac__socket.html#a0f5f24200b0dad4ef1443b863d1bfc7d',1,'guac_socket::write_handler()']]]
+];
---
+++ doc/1.2.0/libguac/search/all_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['data',['data',['../structguac__audio__stream.html#a9ca03221b02d1206eb698f8ee936c4b7',1,'guac_audio_stream::data()'],['../structguac__client.html#a0569bc7eea8d37b5bd95bcd3821ca0a3',1,'guac_client::data()'],['../structguac__object.html#ac9ca62cec5ba1124f8272da3adf7c44e',1,'guac_object::data()'],['../structguac__socket.html#ac8340e5627b43138bb50aac6c4368c0d',1,'guac_socket::data()'],['../structguac__stream.html#acdf20063ca408431862d901c9445b429',1,'guac_stream::data()'],['../structguac__user.html#ad424a52c5d996e4a33de5dad61555883',1,'guac_user::data()']]],
+  ['deprecated_20list',['Deprecated List',['../deprecated.html',1,'']]]
+];
