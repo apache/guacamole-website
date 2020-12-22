718441c12dd415897b38c5665fe2ab5c8f054b69
Michael Jumper <mjumper@apache.org>
2020-12-21 16:04:15 -0800

    Add documentation for 1.3.0.

---
+++ doc/1.3.0/libguac/search/files_0.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['argv_2dconstants_2eh',['argv-constants.h',['../argv-constants_8h.html',1,'']]],
+  ['argv_2dfntypes_2eh',['argv-fntypes.h',['../argv-fntypes_8h.html',1,'']]],
+  ['argv_2eh',['argv.h',['../argv_8h.html',1,'']]],
+  ['audio_2dfntypes_2eh',['audio-fntypes.h',['../audio-fntypes_8h.html',1,'']]],
+  ['audio_2dtypes_2eh',['audio-types.h',['../audio-types_8h.html',1,'']]],
+  ['audio_2eh',['audio.h',['../audio_8h.html',1,'']]]
+];
