bd0222a3819908b9472923f3396cff5af9fcf78d
Michael Jumper <mjumper@apache.org>
2023-04-10 11:17:56 -0700

    Add documentation for 1.5.1.

---
+++ doc/1.5.1/libguac/search/all_8.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['image_5fmimetypes_378',['image_mimetypes',['../structguac__user__info.html#ac05b09cce376e656f4e4baa19f888345',1,'guac_user_info']]],
+  ['include_5fkeys_379',['include_keys',['../structguac__recording.html#abe405bddf126b78899464171f838cf35',1,'guac_recording']]],
+  ['include_5fmouse_380',['include_mouse',['../structguac__recording.html#a3a023a7adb2305f0dd1e4e3f12ad37d6',1,'guac_recording']]],
+  ['include_5foutput_381',['include_output',['../structguac__recording.html#a3efc682b40d7989c8e80e9bd7fa73b80',1,'guac_recording']]],
+  ['include_5ftouch_382',['include_touch',['../structguac__recording.html#a3189271cc506aae3d0980e7b2a580f23',1,'guac_recording']]],
+  ['index_383',['index',['../structguac__layer.html#a686a85d9d758ba5f2f4426a4c4ae13ba',1,'guac_layer::index()'],['../structguac__object.html#a643f03695d246bbf73b5c4942755a4b4',1,'guac_object::index()'],['../structguac__stream.html#a487951c2cc77b2e1637e3aa0b5faaa60',1,'guac_stream::index()']]],
+  ['info_384',['info',['../structguac__user.html#a0120f63e0eb933761666047ed80e483f',1,'guac_user']]]
+];
