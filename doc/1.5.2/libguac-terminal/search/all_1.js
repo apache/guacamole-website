fbc35edc86419ffeb22ea2f1e6ff985602fe09c4
Michael Jumper <mjumper@apache.org>
2023-05-19 23:30:11 -0700

    Add documentation for 1.5.2.

---
+++ doc/1.5.2/libguac-terminal/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['color_5fscheme_1',['color_scheme',['../structguac__terminal__options.html#a2a13c286dc09328bdcfea511c40eb776',1,'guac_terminal_options']]]
+];
---
+++ doc/1.5.2/libguac/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['deprecated_20list_921',['Deprecated List',['../deprecated.html',1,'']]]
+];
